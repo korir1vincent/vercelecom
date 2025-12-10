e7a8fddc1b0ab1ea3b6e3eeccd434bcc57187e77
Vincent Korir <korirv09@gmail.com>
2025-12-10 10:25:23 +0300

    urls

--- car1/urls.py
+++ car1/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('mycar.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls'))
-] 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
